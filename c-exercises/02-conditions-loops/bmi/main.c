434bfba46d7d6a69a7c90376354bf6b2bd0bfafd
Konstantin Krasser <konstantin.krasser@icloud.com>
2024-10-23 13:46:39 +0200

    the bmi thing

--- c-exercises/02-conditions-loops/bmi/main.c
+++ c-exercises/02-conditions-loops/bmi/main.c
@@ -8,36 +8,38 @@ int main(void) {
   printf("Bitte geben Sie Ihr Gewicht in kg ein: %d\n", weight);
   /*scanf("%d", &weight);*/
 
-  float hight = 1.8;
-  printf("Bitte geben Sie Ihre Körpergröße in cm ein: %f\n", hight);
+  int hight = 180;
+  printf("Bitte geben Sie Ihre Körpergröße in cm ein: %d\n", hight);
   /* scanf("%d", &hight); */
 
   // Calculate BMI without <math.h> library. Thus it's ^2 which is the same as
   // *2...
 
   // cast hight into a double
-  float BMI = ((weight) / ((hight) * 2));
+  // Why does this not work?
+  /*double bmi = ((weight) / (hight * hight / 1000.0));*/
+
+  double bmi = weight / (hight / 100. * hight / 100.);
   // %LF for Long double, although literature says it's %lf...
-  printf("%f\nKategorie: ", BMI);
+  printf("BMI: %.1f\n", bmi);
 
   // Not that i particulary like switch cases but how could I do this with
   // switch cases?
-  if (BMI < 16.0) {
-    printf("Starkes Untergewicht\n");
-  } else if ((BMI <= 17.0) && (BMI >= 16.0)) {
-    printf("Maessiges Untergewicht\n");
-  } else if ((BMI <= 18.5) && (BMI >= 17.0)) {
-    printf("Leichtes Untergewicht\n");
-  } else if ((BMI <= 25.0) && (BMI >= 18.5)) {
-    printf("Normalgewicht\n");
-  } else if ((BMI <= 30.0) && (BMI >= 25.0)) {
-    printf("Präadipositas\n");
-  } else if ((BMI <= 35.0) && (BMI >= 30.0)) {
-    printf("Adipositas Grad I\n");
-  } else if ((BMI <= 40.0) && (BMI >= 35.0)) {
-    printf("Adipositas Grad II\n");
-  } else if (BMI >= 40.0) {
-    printf("Adipositas Grad III\n");
-  }
+  if (bmi < 16.0)
+    printf("Kategorie: Starkes Untergewicht\n");
+  else if (bmi < 17.0)
+    printf("Kategorie: Mäßiges Untergewicht\n");
+  else if (bmi < 18.5)
+    printf("Kategorie: Leichtes Untergewicht\n");
+  else if (bmi < 25.0)
+    printf("Kategorie: Normalgewicht\n");
+  else if (bmi < 30.0)
+    printf("Kategorie: Präadipositas\n");
+  else if (bmi < 35.0)
+    printf("Kategorie: Adipositas Grad I\n");
+  else if (bmi < 40.0)
+    printf("Kategorie: Adipositas Grad II\n");
+  else
+    printf("Kategorie: Adipositas Grad III\n");
   return 0;
 }
